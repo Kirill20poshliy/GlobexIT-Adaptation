c1c13789128d54cacedc781d976eb9f244143ce0
KirillYusikov <109421695+Kirill20poshliy@users.noreply.github.com>
2024-07-17 11:40:31 +0300

    fix(#1): Правки

--- uploadings/src/import excel/uploading_colls_and_pos_excel.ts
+++ uploadings/src/import excel/uploading_colls_and_pos_excel.ts
@@ -2,32 +2,43 @@ declare let continueFlag: boolean
 
 // Этап 1. Выгрузка новых пользователей. Регулярно выполняемый код:
 //
-var collsArray = ArrayOptFirstElem(XQuery("sql: SELECT id FROM dbo.collaborators WHERE fullname = '"
-    + Trim('{[2]}') + " " 
-    + Trim('{[3]}') + " "
-    + Trim('{[4]}') + "'"))
-if (collsArray != undefined || Trim('{[2]}') == "") {
+let arrColls = ArrayOptFirstElem(XQuery("sql: SELECT id FROM dbo.collaborators WHERE code = '" + Trim('{[1]}') + "'"))
+if (arrColls != undefined || Trim('{[1]}') == "") {
     continueFlag = true
 }
 
 
 // Этап 2. Обновление пользователей. Регулярно выполняемый код:
 //
-if (Trim('{[2]}') == "" && Trim('{[3]}') == "") {
+if (Trim('{[1]}') == "") {
     continueFlag = true
 }
 
 
 // Этап 3. Выгрузка новых должностей. Регулярно выполняемый код:
 //
-var possArray = ArrayOptFirstElem(XQuery("sql: SELECT id FROM dbo.positions WHERE name = '"+ Trim('{[8]}' + "'")))
-if (possArray != undefined || Trim('{[8]}')=="") {
+if (Trim('{[5]}') == "") {
     continueFlag = true
+}
+
+let arrPoss = ArraySelectAll<{code: string}>(XQuery("sql: SELECT c.code \
+    FROM dbo.positions p \
+    JOIN dbo.collaborators c ON c.id = p.basic_collaborator_id \
+    WHERE p.name = '" + Trim('{[8]}') + "' AND p.code = '"+ Trim('{[5]}') + "'"))
+
+if (ArrayCount(arrPoss) !== 0) {
+    let i: number = 0
+    for (i; i < ArrayCount(arrPoss); i++) {
+        if (arrPoss[i].code == Trim('{[1]}')) {
+            continueFlag = true
+            break
+        }
+    }
 }
 
 
 // Этап 4. Обновление должностей. Регулярно выполняемый код:
 //
-if (Trim('{[8]}') == "") {
+if (Trim('{[5]}') == "") {
     continueFlag = true
 }
