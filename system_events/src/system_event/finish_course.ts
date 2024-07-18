e2ab0ee259258c48606b069f75a4360d3cec7485
KirillYusikov <109421695+Kirill20poshliy@users.noreply.github.com>
2024-07-18 11:51:17 +0300

    task(#13): обработчик события завершения курса

---
+++ system_events/src/system_event/finish_course.ts
@@ -0,0 +1,23 @@
+declare let teSystemEventHandler: {wvars: Object}
+declare let learningDoc: XmlTopElem
+
+
+let newCourseId = teSystemEventHandler.wvars.ObtainChildByKey( "newCourseId" ).value
+
+
+let learningCourseId = learningDoc.Child('course_id').Value
+let learningPersonId = learningDoc.Child('person_id').Value
+let learningStateId = learningDoc.Child('state_id').Value
+
+
+let arrLearnings = ArraySelectAll(XQuery("sql: SELECT id FROM dbo.learnings \
+    WHERE course_id = '" + learningCourseId + "' AND person_id = '" + learningPersonId + "' AND state_id <> 4"))
+
+
+if (ArrayCount(arrLearnings) < 3 && learningStateId == 4) {
+    try {
+        tools.activate_course_to_person(learningPersonId + '', newCourseId)
+    } catch (e) {
+        alert('Ошибка при назначении курса: ' + e?.mesage)
+    }
+}
