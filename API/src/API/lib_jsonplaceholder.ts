a324213bd3bc6301d234834a64d7cafbe437aec8
KirillYusikov <109421695+Kirill20poshliy@users.noreply.github.com>
2024-07-23 10:18:08 +0300

    task(#15): API jsonplaceholder

---
+++ API/src/API/lib_jsonplaceholder.ts
@@ -0,0 +1,27 @@
+﻿/** 
+ * @namespace Websoft.WT.JSONplaceholder 
+*/
+
+/**
+ * @function Get_todos
+ * @memberof Websoft.WT.JSONplaceholder
+ * @description Получает список todos с сервиса jsonplaceholder
+ * @returns {Object}
+*/
+
+
+function Get_todos(): {code: number, data: []} {
+    try {
+        var result = HttpRequest('https://jsonplaceholder.typicode.com/todos')
+        return {
+            code: result.RespCode,
+            data: tools.read_object(result.Body)
+        }
+    } catch (e) {
+        alert(e.message)
+        return {
+            code: result.RespCode,
+            data: []
+        }
+    }
+}
