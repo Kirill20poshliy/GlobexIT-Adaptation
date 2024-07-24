1cd6be9cf7a6fe5324d9a3a6d204256c90c3854c
KirillYusikov <109421695+Kirill20poshliy@users.noreply.github.com>
2024-07-24 13:34:56 +0300

    task(#18): Библиотека программного кода и агент для её вызова

---
+++ code_lib/src/agnt_call_lib/agnt_call_lib_method.ts
@@ -0,0 +1,71 @@
+﻿let IS_DEBUG = tools_web.is_true(Param.IS_DEBUG)
+const MODE = Param.GetOptProperty('MODE')
+let userId = Param.GetOptProperty('userId')
+let groupId = Param.GetOptProperty('groupId')
+let notificationTypeId = Param.GetOptProperty('notificationTypeId')
+let percent = Param.GetOptProperty('percent')
+
+
+function callLib() {
+    try {
+        switch (MODE) {
+            case 'Сменить пароль':
+                if (!userId || !notificationTypeId) 
+                    throw new Error('Не указаны требуемые параметры для выполнения метода!')
+                tools.call_code_library_method('lib_collaborator', 'changePassword', [userId, notificationTypeId])
+                break
+            case 'Повысить зарплату':
+                if (!userId || !percent) 
+                    throw new Error('Не указаны требуемые параметры для выполнения метода!')
+                tools.call_code_library_method('lib_collaborator', 'raiseSalary', [userId, OptInt(percent, 0)])
+                break
+            case 'Добавить в группу':
+                if (!userId || !groupId)
+                    throw new Error('Не указаны требуемые параметры для выполнения метода!')
+                tools.call_code_library_method('lib_collaborator', 'addInGroup', [userId, groupId])
+                break
+            case 'Уволить':
+                if (!userId) 
+                    throw new Error('Не указаны требуемые параметры для выполнения метода!')
+                tools.call_code_library_method('lib_collaborator', 'dismiss', [userId])
+                break
+            default: throw new Error('Указан неверный MODE!')
+        }
+    } catch (e) {
+        throw Error("callLib -> " + e.message)
+    }
+}
+
+
+function Main() {
+    try {
+        callLib()
+    } catch (e) {
+        throw Error("Main -> " + e.message)
+    }
+}
+
+
+function Log(value1: string, value2?: string) {
+    var text = value2 != undefined ? value1 + ": " + value2 : value1
+    if(IS_DEBUG)
+        alert(text)
+} 
+
+
+Log("----------------Начало. Globexit. Адаптация. #18. Агент «Вызов методов из библиотеки кода»------------------")
+
+var mainTimer = DateToRawSeconds(Date()) 
+
+try {
+    Main();
+} catch(e) {
+    Log(e?.message)
+} 
+
+mainTimer = DateToRawSeconds(Date()) - mainTimer
+
+Log("Агент завершил свою работу за " + mainTimer + " секунд")
+Log("---------------------Конец. Адаптация. #18. Агент «Вызов методов из библиотеки кода»-----------------------")
+
+export {}
---
+++ code_lib/src/lib_collaborator/lib_collaborator.ts
@@ -0,0 +1,66 @@
+﻿function changePassword(userId: number, notificationTypeId: number) {
+    try {
+        let oDoc = tools.open_doc(userId)
+        if (oDoc == undefined) {
+            throw new Error('Сотрудника с ID ' + userId + ' не существует!')
+        }
+        let newPass = tools.random_string(12)
+    
+        oDoc.TopElem.Child('password').Value = newPass
+        oDoc.Save()
+    
+        tools.create_notification(notificationTypeId, userId, newPass, null, oDoc.TopElem)
+    } catch (e) {
+        alert('changePassword -> ' + e.message)
+    }
+}
+
+
+function raiseSalary(userId: number, percent: number) {
+    try {
+        let oDoc = tools.open_doc(userId)
+        if (oDoc == undefined) {
+            throw new Error('Сотрудника с ID ' + userId + ' не существует!')
+        }
+        let salary = oDoc.TopElem.Child('comp_ben').Child('salary').Value
+        let newSalary = OptReal(salary) * (1 + OptReal(percent) / 100)
+    
+        oDoc.TopElem.Child('comp_ben').Child('salary').Value = newSalary
+        oDoc.Save()
+    } catch (e) {
+        alert('raiseSalary -> ' + e.message)
+    }
+}
+
+
+function addInGroup(userId: number, groupId: number) {
+    try {
+        if (!userId) {
+            throw new Error('ID сотрудника не задан!')
+        }
+        let grDoc = tools.open_doc(groupId)
+        if (grDoc == undefined) {
+            throw new Error('Группы с ID ' + groupId + ' не существует!')
+        }
+        let newColl = grDoc.TopElem.Child('collaborators').AddChild('collaborator')
+        newColl.Child('collaborator_id').Value = userId
+        grDoc.Save()
+    } catch (e) {
+        alert('addInGroup -> ' + e.message)
+    }
+}
+
+
+function dismiss(userId: number) {
+    try {
+        let oDoc = tools.open_doc(userId)
+        if (oDoc == undefined) {
+            throw new Error('Сотрудника с ID ' + userId + ' не существует!')
+        }
+        oDoc.TopElem.Child('is_dismiss').Value = 1
+        oDoc.TopElem.Child('dismiss_date').Value = Date()
+        oDoc.Save()
+    } catch (e) {
+        alert('dismiss -> ' + e.message)
+    }
+}
