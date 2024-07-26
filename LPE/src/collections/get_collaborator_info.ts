d89227edc4fbe89cb98d0a47fd61d404fd4f02b1
KirillYusikov <109421695+Kirill20poshliy@users.noreply.github.com>
2024-07-26 12:30:38 +0300

    task(#20): Выборки и удалённое действие для страницы в редакторе

---
+++ LPE/src/collections/get_collaborator_info.ts
@@ -0,0 +1,69 @@
+﻿interface iCollInfo {
+	email: string,
+	age: number,
+	birth_date: string,
+	position_name: string,
+	position_parent_name: string,
+	tab_num: string
+}
+
+
+interface iResult {
+    id: number, 
+    name: string, 
+    value: string | number
+}
+
+
+declare let RESULT: iResult[]
+
+
+let collInfo = ArrayOptFirstElem<iCollInfo>(XQuery("sql: \
+    SELECT \
+        c1.email, \
+        date_part('year', age(c1.birth_date)) AS age, \
+        c1.birth_date, \
+        c1.position_name, \
+        c1.position_parent_name, \
+        tab_num \
+    FROM dbo.collaborators c1 \
+    JOIN dbo.collaborator c2 ON c1.id = c2.id \
+    CROSS JOIN LATERAL unnest(xpath('/collaborator/custom_elems/custom_elem[name=\"tab_num\"]/value/text()', c2.data)::text[]) AS tab_num \
+    WHERE c1.id = " + curUserID
+))
+
+
+RESULT = [
+    {
+        id: 1,
+        name: 'Email',
+        value: collInfo.email
+    },
+    {
+        id: 2,
+        name: 'Возраст',
+        value: collInfo.age
+    },
+    {
+        id: 3,
+        name: 'Дата рождения',
+        value: StrDate(Date(collInfo.birth_date), false)
+    },
+    {
+        id: 4,
+        name: 'Должность',
+        value: collInfo.position_name
+    },
+    {
+        id: 5,
+        name: 'Подразделение',
+        value: collInfo.position_parent_name
+    },
+    {
+        id: 6,
+        name: 'Табельный номер',
+        value: collInfo.tab_num
+    }
+];
+
+export {}
---
+++ LPE/src/collections/get_collaborator_managers.ts
@@ -0,0 +1,29 @@
+﻿interface iManager {
+    id: number,
+    person_id?: number,
+    person_fullname: string,
+    pict_url: string,
+}
+
+declare let RESULT: iManager[]
+
+let arrManagers = ArraySelectAll<iManager>(XQuery("sql: \
+    SELECT fm.person_id, fm.person_fullname, c.pict_url \
+    FROM dbo.func_managers fm \
+    JOIN dbo.collaborators c ON fm.person_id = c.id \
+    WHERE fm.object_id = " + curUserID
+))
+
+
+RESULT = []
+let i: number = 0
+for (i; i < ArrayCount(arrManagers); i++) {
+    RESULT.push({
+        id: arrManagers[i].person_id,
+        person_fullname: arrManagers[i].person_fullname,
+        pict_url: UrlAppendPath(global_settings.settings.portal_base_url, arrManagers[i].pict_url)
+    })
+}
+
+
+export {}
